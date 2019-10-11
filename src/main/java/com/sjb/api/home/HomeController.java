3af51166184f67bffe998b946c4276f6cba3a126
simjunbo <s860620@gmail.com>
2019-10-11 10:44:06 +0900

    controller edit

---
+++ src/main/java/com/sjb/api/home/HomeController.java
@@ -0,0 +1,34 @@
+package com.sjb.api.home;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ModelAndView helloWorldToView() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        return mv;
+    }
+
+    @RequestMapping(value = "/hello.string", method = RequestMethod.GET)
+    public String helloWorldToString() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/hello.json", method = RequestMethod.GET)
+    public Map<String, String> helloWorldToJson() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "aiden");
+        return map;
+    }
+}
+
